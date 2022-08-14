6fe1378d439be49a733c3666bf1990ba84472ebc
BrettLennox <brettheaton@hotmail.com>
2022-08-14 21:25:18 +1000

    Bullet LookTowards sort of

--- Assets/Scripts/Player/BulletBehaviour.cs
+++ Assets/Scripts/Player/BulletBehaviour.cs
@@ -7,7 +7,10 @@ public class BulletBehaviour : MonoBehaviour
     #region Variables
     [SerializeField] private GameObject _target;
     [SerializeField] private float _speed;
+    [SerializeField] private float _offset = 1f;
     Vector3 dir;
+    Vector3 _targetPos, _thisPos;
+    float _angle;
     #endregion
     #region Properties
     public GameObject Target { get => _target; set => _target = value; }
@@ -16,6 +19,13 @@ public class BulletBehaviour : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        MoveTowardsTarget();
+
+        transform.right = Target.transform.position - transform.position;
+    }
+
+    private void MoveTowardsTarget()
     {
         dir = Target.transform.position - transform.position;
         dir.Normalize();
